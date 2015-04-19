475f67ad141bd12b8f2aa3d18324ceef047f2dee
korove <undfined1@gmail.com>
2015-04-19 20:37:11 +0700

    new layout for admin

--- admin/homeAdm.php
+++ admin/homeAdm.php
@@ -1,30 +1,35 @@
 <?php 
 	$ds = DIRECTORY_SEPARATOR;
 	$base_dir = realpath(dirname(__FILE__)  . $ds . '..') . $ds;
-	echo "{$ds}<br/>";
-	echo "{$base_dir}<br/>";
-	echo "{$base_dir}include{$ds}function.php<br/>";
-	include("{$base_dir}include{$ds}function.php");
+	// echo "{$ds}<br/>";
+	// echo "{$base_dir}<br/>";
+	// echo "{$base_dir}include{$ds}function.php<br/>";
+	//include_once("{$base_dir}include{$ds}function.php");
+	// include_once("{$ds}nommali{$ds}include{$ds}function.php");
+	$root = $_SERVER['DOCUMENT_ROOT'];
+   	$path = $root . "nommali/include/function.php";
+   	include_once($path);
+
 ?>
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Admin page</title>
-	<link rel="stylesheet" href="./bootstrap-3.3.4/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/nommali/bootstrap-3.3.4/dist/css/bootstrap.min.css">
 
 	<!-- Optional theme -->
-	<link rel="stylesheet" href="./bootstrap-3.3.4/dist/css/bootstrap-theme.min.css">
-	<link rel="stylesheet" href="./jquery-ui-1.11.4.custom/jquery-ui.css">
+	<link rel="stylesheet" href="/nommali/bootstrap-3.3.4/dist/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="/nommali/jquery-ui-1.11.4.custom/jquery-ui.css">
 
-	<script src="./js/jquery-2.1.1.min.js"></script>
-	<script src="./js/jquery.blockUI.js"></script>
-	<script src="./js/jquery.form.min.js"></script>
-	<script src="./bootstrap-3.3.4/dist/js/bootstrap.min.js"></script>
-	<script src="./jquery-ui-1.11.4.custom/jquery-ui.js"></script>
-	<link rel="stylesheet" type="text/css" href="./css/home.css">
-	<link rel="stylesheet" type="text/css" href="./css/admin.css">
+	<script src="/nommali/js/jquery-2.1.1.min.js"></script>
+	<script src="/nommali/js/jquery.blockUI.js"></script>
+	<script src="/nommali/js/jquery.form.min.js"></script>
+	<script src="/nommali/bootstrap-3.3.4/dist/js/bootstrap.min.js"></script>
+	<script src="/nommali//jquery-ui-1.11.4.custom/jquery-ui.js"></script>
+	<link rel="stylesheet" type="text/css" href="/nommali/css/home.css">
+	<link rel="stylesheet" type="text/css" href="/nommali/css/admin.css">
 	
-	<script src="./js/homeScript.js"></script>
+	<script src="/nommali/js/homeScript.js"></script>
 	<script>
 		// $(function(){
 		// 	var welcomeAdminPage=location.pathname+'admin/welcomeAdmin.php';
@@ -43,20 +48,34 @@
 			<div ><img src="/nommali/logomali.jpg" style="width:120px;" /></div>
 		</div>
 
-
+		<div id="navAdm">
+			<?php include "{$base_dir}headerAdm.php"; ?>
+		</div>
 
 		<div id="contentAdm">
 			<?php echo __DIR__ ?><br>
 			<?php echo __FILE__ ?>
 			<div style="width:600px;">
-			</div>
 
+			</div>
+			<?php
+
+				$page = '';
+				if(isset($_GET['page'])){
+					$page = $_GET['page'];
+				}
+				
+				if (!empty($page) && ($page != 'contentAdm')) {
+					$page .= '.php';
+					include($root . "nommali/admin/". $page);
+				} else {
+					echo 'this is admin home content';
+				}
+			?>
 		</div>
 
-
-
 		<div id="footerAdm">
-			<?php include("{$base_dir}footerAdm.php"); ?>
+			<?php include("{$base_dir}admin{$ds}footerAdm.php"); ?>
 		</div>
 
 	</div>
---
+++ headerAdm.php
@@ -0,0 +1,12 @@
+<div >
+	<ul class="nav nav-tabs">
+	  <li role="presentation" class="active" ><a href="homeAdm.php?page=contentAdm" class="glyphicon glyphicon-home"> Home</a></li>
+	  <li role="presentation"><a href="homeAdm.php?page=productAdm" class="glyphicon glyphicon-briefcase"> Product</a></li>
+	  <li role="presentation"><a href="homeAdm.php?page=activityAdm" class=" glyphicon glyphicon-bullhorn"> Activity</a></li>
+	  <li role="presentation"><a href="homeAdm.php?page=csrAdm" class="glyphicon glyphicon-tree-deciduous"> CSR</a></li>
+	  <li role="presentation"><a href="homeAdm.php?page=newsAdm" class="glyphicon glyphicon-comment"> ข่าวสาร</a></li>
+	  <li role="presentation"><a href="homeAdm.php?page=movieAdm" class="glyphicon glyphicon-facetime-video"> ภาพยนตร์</a></li>
+	  <li role="presentation"><a href="homeAdm.php?page=joinUsAdm" class="glyphicon glyphicon-thumbs-up"> ร่วมงานกับเรา</a></li>
+	  <li role="presentation"><a href="homeAdm.php?page=faqAdm" class="glyphicon glyphicon-question-sign"> FAQ</a></li>
+	</ul>
+</div>
