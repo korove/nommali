3793f5bbe90c233655a80d03b74d54aa51bd8d54
korove <undfined1@gmail.com>
2015-06-20 19:08:45 +0700

    index.php

--- mali/index.php
+++ mali/index.php
@@ -1,4 +1,18 @@
 <!DOCTYPE html>
+<?php 
+	$ds = DIRECTORY_SEPARATOR;
+	$base_dir = realpath(dirname(__FILE__)  . $ds . '..') . $ds;
+	// echo "{$ds}<br/>";
+	// echo "{$base_dir}<br/>";
+	// echo "{$base_dir}include{$ds}function.php<br/>";
+	//include_once("{$base_dir}include{$ds}function.php");
+	// include_once("{$ds}nommali{$ds}include{$ds}function.php");
+	$root = $_SERVER['DOCUMENT_ROOT'];
+	$includePath = "nommali/include/";
+   	$path = $root . $includePath ."function.php";
+   	include_once($path);
+
+?>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -181,111 +195,7 @@
 				</div>
 				<!-- Content -->
 				<div class="main-content">
-					<div class="box2">
-						<div class="column-6">
-							<div id="about-logo"></div>
-						</div>
-						<div class="column-6">
-							<h1><a href="#">About Us</a></h1>
-							<p>	was set up in 1962 as a joint venture between Thai and Malaysian businessmen 
-							with the Australian Dairy Corporation and was granted investment promotion privileges
-							by the Board of Investment. The goal of the company was to manufacture dairy products
-							thai milk. was set up in 1962 as a joint venture between Thai and Malaysian businessmen 
-							with the Australian Dairy Corporation .</p>
-						</div>
-					</div><div class="clear"></div>
-					
-					<div class="box3">
-						<div class="red-arrow"></div> 
-						<div class="column-1">
-							<div class="box3-header">
-								<h2>ข่าว</h2>
-								<a href="#"><div class="more">MORE +</div></a>
-							</div>
-							<div class="box3-content1">
-								<img src="news/news1.png" height="10" width="10">
-								<p>บริษัทอุตสาหกรรมไทยได้รับรางวัลชั้นน้ำ</p>
-								<a href="#">อ่านต่อ</a>
-							</div>
-							<div class="box3-content1">
-								<img src="news/news2.png">
-								<p>บริษัทอุตสาหกรรมไทยได้รับรางวัลชั้นน้ำ</p>
-								<a href="#">อ่านต่อ</a>
-							</div>
-							<div class="box3-content1">
-								<img src="news/news3.png">
-								<p>บริษัทอุตสาหกรรมไทยได้รับรางวัลชั้นน้ำ</p>
-								<a href="#">อ่านต่อ</a>
-							</div>
-							<div class="box3-content1">
-								<img src="news/news4.png">
-								<p>บริษัทอุตสาหกรรมไทยได้รับรางวัลชั้นน้ำ</p>
-								<a href="#">อ่านต่อ</a>
-							</div>
-							<div class="box3-content1">
-								<img src="news/news5.png">
-								<p>บริษัทอุตสาหกรรมไทยได้รับรางวัลชั้นน้ำ</p>
-								<a href="#">อ่านต่อ</a>
-							</div>
-							<div class="box3-content1">
-								<img src="news/news6.png">
-								<p>บริษัทอุตสาหกรรมไทยได้รับรางวัลชั้นน้ำ</p>
-								<a href="#">อ่านต่อ</a>
-							</div>
-						</div>
-						<div class="column-2">
-							<div class="box3-header">
-								<h2>VDO ข่าว</h2>
-								<a href="#"><div class="more">MORE +</div></a><div class="clear"></div>
-							</div>
-							<div class="box3-content2">
-								<h3>นมตรามะลิได้ร่วมแสดงความยินดี...</h3>
-								<img src="news/video1.png">
-								<p>เมื่อวันที่ 23 มีนาคม 2558 ที่ผ่านมา คุณสุดถนอม กรรณสูด รองกรรมการผู้จัดการ
-								นมตรามะลิได้ร่วมแสดงความยินดีกับหนังสือพิมพ์แนวหน้า</p>
-								<a href="#">ดูคลิปวีดีโอ</a>
-							</div>
-						</div>
-						<div class="clear"></div>
-					</div>
-					
-					
-					<div class="box3">
-						<div class="red-arrow"></div> 
-						<div class="column-1">
-							<div class="box3-header">
-								<h2>กิจกรรม</h2>
-								<a href="#"><div class="more">MORE +</div></a>
-							</div>
-							<div class="box3-content1">
-								<img src="news/news1.png">
-								<p>บริษัทอุตสาหกรรมไทยได้รับรางวัลชั้นน้ำ</p>
-								<a href="#">อ่านต่อ</a>
-							</div>
-							<div class="box3-content1">
-								<img src="news/news2.png">
-								<p>บริษัทอุตสาหกรรมไทยได้รับรางวัลชั้นน้ำ</p>
-								<a href="#">อ่านต่อ</a>
-							</div>
-							<div class="box3-content1">
-								<img src="news/news3.png">
-								<p>บริษัทอุตสาหกรรมไทยได้รับรางวัลชั้นน้ำ</p>
-								<a href="#">อ่านต่อ</a>
-							</div>
-						</div>
-						<div class="column-2">
-							<div class="box3-header">
-								<h2>Recipes VDO</h2>
-								<a href="#"><div class="more">MORE +</div></a><div class="clear"></div>
-							</div>
-							<div class="box3-content2">
-								<img src="news/video2.png">
-								<p>เมนูอาหารแนะนำกับผลิตภัณฑ์ตรามะลิ </p>
-								<a href="#">ดูคลิปวีดีโอ</a>
-							</div>
-						</div>
-						<div class="clear"></div>
-					</div>
+					<?php include "{$base_dir}mali{$ds}mainHomeContent.php"; ?>
 				</div>
 			</div>
 			<div class="clear"></div>
---
+++ mali/mainHomeContent.php
@@ -0,0 +1,112 @@
+<div class="box2">
+	<div class="column-6">
+		<div id="about-logo"></div>
+	</div>
+	<div class="column-6">
+		<h1>
+			<a href="#">About Us</a>
+		</h1>
+		<p>was set up in 1962 as a joint venture between Thai and Malaysian
+			businessmen with the Australian Dairy Corporation and was granted
+			investment promotion privileges by the Board of Investment. The goal
+			of the company was to manufacture dairy products thai milk. was set
+			up in 1962 as a joint venture between Thai and Malaysian businessmen
+			with the Australian Dairy Corporation .</p>
+	</div>
+</div>
+<div class="clear"></div>
+
+<div class="box3">
+	<div class="red-arrow"></div>
+	<div class="column-1">
+		<div class="box3-header">
+			<h2>ข่าว</h2>
+			<a href="#"><div class="more">MORE +</div></a>
+		</div>
+		<div class="box3-content1">
+			<img src="news/news1.png" height="10" width="10">
+			<p>บริษัทอุตสาหกรรมไทยได้รับรางวัลชั้นน้ำ</p>
+			<a href="#">อ่านต่อ</a>
+		</div>
+		<div class="box3-content1">
+			<img src="news/news2.png">
+			<p>บริษัทอุตสาหกรรมไทยได้รับรางวัลชั้นน้ำ</p>
+			<a href="#">อ่านต่อ</a>
+		</div>
+		<div class="box3-content1">
+			<img src="news/news3.png">
+			<p>บริษัทอุตสาหกรรมไทยได้รับรางวัลชั้นน้ำ</p>
+			<a href="#">อ่านต่อ</a>
+		</div>
+		<div class="box3-content1">
+			<img src="news/news4.png">
+			<p>บริษัทอุตสาหกรรมไทยได้รับรางวัลชั้นน้ำ</p>
+			<a href="#">อ่านต่อ</a>
+		</div>
+		<div class="box3-content1">
+			<img src="news/news5.png">
+			<p>บริษัทอุตสาหกรรมไทยได้รับรางวัลชั้นน้ำ</p>
+			<a href="#">อ่านต่อ</a>
+		</div>
+		<div class="box3-content1">
+			<img src="news/news6.png">
+			<p>บริษัทอุตสาหกรรมไทยได้รับรางวัลชั้นน้ำ</p>
+			<a href="#">อ่านต่อ</a>
+		</div>
+	</div>
+	<div class="column-2">
+		<div class="box3-header">
+			<h2>VDO ข่าว</h2>
+			<a href="#"><div class="more">MORE +</div></a>
+			<div class="clear"></div>
+		</div>
+		<div class="box3-content2">
+			<h3>นมตรามะลิได้ร่วมแสดงความยินดี...</h3>
+			<img src="news/video1.png">
+			<p>เมื่อวันที่ 23 มีนาคม 2558 ที่ผ่านมา คุณสุดถนอม กรรณสูด
+				รองกรรมการผู้จัดการ
+				นมตรามะลิได้ร่วมแสดงความยินดีกับหนังสือพิมพ์แนวหน้า</p>
+			<a href="#">ดูคลิปวีดีโอ</a>
+		</div>
+	</div>
+	<div class="clear"></div>
+</div>
+
+
+<div class="box3">
+	<div class="red-arrow"></div>
+	<div class="column-1">
+		<div class="box3-header">
+			<h2>กิจกรรม</h2>
+			<a href="#"><div class="more">MORE +</div></a>
+		</div>
+		<div class="box3-content1">
+			<img src="news/news1.png">
+			<p>บริษัทอุตสาหกรรมไทยได้รับรางวัลชั้นน้ำ</p>
+			<a href="#">อ่านต่อ</a>
+		</div>
+		<div class="box3-content1">
+			<img src="news/news2.png">
+			<p>บริษัทอุตสาหกรรมไทยได้รับรางวัลชั้นน้ำ</p>
+			<a href="#">อ่านต่อ</a>
+		</div>
+		<div class="box3-content1">
+			<img src="news/news3.png">
+			<p>บริษัทอุตสาหกรรมไทยได้รับรางวัลชั้นน้ำ</p>
+			<a href="#">อ่านต่อ</a>
+		</div>
+	</div>
+	<div class="column-2">
+		<div class="box3-header">
+			<h2>Recipes VDO</h2>
+			<a href="#"><div class="more">MORE +</div></a>
+			<div class="clear"></div>
+		</div>
+		<div class="box3-content2">
+			<img src="news/video2.png">
+			<p>เมนูอาหารแนะนำกับผลิตภัณฑ์ตรามะลิ</p>
+			<a href="#">ดูคลิปวีดีโอ</a>
+		</div>
+	</div>
+	<div class="clear"></div>
+</div>
